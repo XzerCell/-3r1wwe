6a5319d7adaece7a25d5f174eadd78a1765db36c
XzerCell <128249345+XzerCell@users.noreply.github.com>
2023-04-07 15:11:07 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,53 @@
+import discord
+import random
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Привет! Я бот!')
+    elif message.content.startswith('$smile'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send("Я не понимаю такую команду!")
+
+
+
+client.run("MTA5MTc4MDY3NDAzNTM5Njc1MA.G3ZpMg.aGy9XtRv5pcKk7Ty8CGWEY0UexHOE8QH7fYq5E")
